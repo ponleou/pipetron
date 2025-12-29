a0cd3fadb5234cf60be71935909024e6228269cf
ponleou <dev.ponleousk@gmail.com>
2025-12-29 21:58:40 +1100

    docs: test files

--- tests/playground.cpp
+++ tests/playground.cpp
@@ -123,3 +123,88 @@ int main() {
 
     return 0;
 }
+
+/*
+static void proxy_info_event_add_virtual_streams(void *data, const struct pw_node_info *info) {
+    if (info->props) {
+        auto *reg_data = (struct registry_event_global_data *)data;
+
+        struct pw_properties *context_props = pw_properties_new(PW_KEY_APP_NAME, "Your App Name", nullptr);
+        struct pw_properties *stream_props =
+            pw_properties_new(PW_KEY_MEDIA_TYPE, "Audio", PW_KEY_APP_NAME, "Your App Name", nullptr);
+        struct pw_context *virutal_context =
+            pw_context_new(pw_main_loop_get_loop(reg_data->main_loop), context_props, 0);
+        struct pw_core *virtual_core = pw_context_connect(virutal_context, nullptr, 0);
+        struct pw_stream *stream = pw_stream_new(virtual_core, "Playback", stream_props);
+
+        uint8_t buffer[1024];
+        struct spa_pod_builder b = SPA_POD_BUILDER_INIT(buffer, sizeof(buffer));
+        struct spa_audio_info_raw a_info =
+            SPA_AUDIO_INFO_RAW_INIT(.format = SPA_AUDIO_FORMAT_F32, .rate = 48000, .channels = 2);
+
+        const struct spa_pod *params[1];
+        params[0] = spa_format_audio_raw_build(&b, SPA_PARAM_EnumFormat, &a_info);
+
+        pw_stream_connect(stream, PW_DIRECTION_OUTPUT, PW_ID_ANY,
+                          (enum pw_stream_flags)(PW_STREAM_FLAG_AUTOCONNECT | PW_STREAM_FLAG_MAP_BUFFERS), params, 1);
+
+        const struct spa_dict_item *item;
+        spa_dict_for_each(item, info->props) { printf("%s = %s\n", item->key, item->value); }
+        cout << endl;
+    }
+}
+
+// TODO: currently checking volumes
+static void proxy_param_props_event(void *data, int seq, uint32_t id, uint32_t index, uint32_t next,
+                                    const struct spa_pod *param) {
+
+    if (id == SPA_PARAM_Format) {
+        struct spa_audio_info_raw info = {};
+        spa_format_audio_raw_parse(param, &info);
+
+        printf("Rate: %u\n", info.rate);
+        printf("Channels: %u\n", info.channels);
+        printf("Format: %u\n", info.format);
+        printf("Flags: %u\n", info.flags);
+        printf("Position: ");
+        for (uint32_t i = 0; i < info.channels; i++) {
+            printf("%u ", info.position[i]);
+        }
+        printf("\n");
+    }
+
+    if (id == SPA_PARAM_Props) {
+        float volume = 0.0f;
+        bool mute = false;
+        struct spa_pod *channel_vols = nullptr;
+        struct spa_pod *channel_map = nullptr;
+
+        spa_pod_parse_object(param, SPA_TYPE_OBJECT_Props, NULL, SPA_PROP_volume, SPA_POD_OPT_Float(&volume),
+                             SPA_PROP_mute, SPA_POD_OPT_Bool(&mute), SPA_PROP_channelVolumes,
+                             SPA_POD_OPT_Pod(&channel_vols), SPA_PROP_channelMap, SPA_POD_OPT_Pod(&channel_map));
+
+        printf("volume: %f, mute: %d\n", volume, mute);
+
+        if (channel_vols) {
+            uint32_t n_vals;
+            float *vals = (float *)spa_pod_get_array(channel_vols, &n_vals);
+            printf("channelVolumes: ");
+            for (uint32_t i = 0; i < n_vals; i++) {
+                printf("%f ", vals[i]);
+            }
+            printf("\n");
+        }
+
+        if (channel_map) {
+            uint32_t n_channels;
+            uint32_t *channels = (uint32_t *)spa_pod_get_array(channel_map, &n_channels);
+            printf("channelMap: ");
+            for (uint32_t i = 0; i < n_channels; i++) {
+                const char *name = spa_debug_type_find_name(spa_type_audio_channel, channels[i]);
+                printf("%s ", name ? name : "UNKNOWN");
+            }
+            printf("\n");
+        }
+    }
+}
+    */
