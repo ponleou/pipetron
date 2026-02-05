260e30deccb3a3edd6b1be65624ec1f447e2e781
ponleou <dev.ponleousk@gmail.com>
2026-02-05 16:34:08 +1100

    feat: added lock to daemon

--- src/includes/daemon.hpp
+++ src/includes/daemon.hpp
@@ -9,55 +9,73 @@
 #include "spa/utils/dict.h"
 #include "spa/utils/hook.h"
 #include <cerrno>
+#include <config.h>
 #include <cstdint>
+#include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <ostream>
 #include <spa/param/audio/format-utils.h>
 #include <string>
+#include <sys/file.h>
+using std::cerr;
 using std::cout;
 using std::endl;
 using std::string;
 
-namespace {
-void raiseError(bool condition, string message, int status = 1) {
-    if (condition) {
-        cout << "Error: " << message << endl;
-        exit(status);
-    }
-}
+class Daemon {
+    struct registry_event_global_data {
+        struct pw_main_loop *main_loop;
+        struct pw_registry *reg;
+    };
 
-struct registry_event_global_data {
-    struct pw_main_loop *main_loop;
-    struct pw_registry *reg;
-};
-
-static void reg_event_find_chromium_nodes(void *data, uint32_t id, uint32_t permissions, const char *type,
-                                          uint32_t version, const struct spa_dict *props) {
-
-    auto *reg_data = (struct registry_event_global_data *)data;
-
-    if (strcmp(type, PW_TYPE_INTERFACE_Node) == 0) {
-        for (uint32_t i = 0; i < props->n_items; i++) {
-
-            if (strcmp(props->items[i].key, PW_KEY_APP_NAME) != 0)
-                continue;
-
-            if (strcmp(props->items[i].value, "Chromium") == 0 ||
-                strcmp(props->items[i].value, "Chromium input") == 0) {
-
-                NodesManager::process_new_node(reg_data->reg, pw_main_loop_get_loop(reg_data->main_loop), id, type);
-            }
-
-            break;
+  private:
+    static void raise_error(bool condition, string message, int status = 1) {
+        if (condition) {
+            cerr << "Error: " << message << endl;
+            exit(status);
         }
     }
-}
-} // namespace
 
-class Daemon {
+    static void checkLock() {
+        const char *runtime_dir = getenv("XDG_RUNTIME_DIR");
+        raise_error(!runtime_dir, "XDG_RUNTIME_DIR not set");
+
+        string lock_file = string(runtime_dir) + "/" + PROJECT_NAME + ".lock";
+
+        int file = open(lock_file.c_str(), O_CREAT | O_RDWR, 0644);
+        raise_error(file == -1, string("Open ") + lock_file + ": " + strerror(errno), errno);
+
+        raise_error(flock(file, LOCK_EX | LOCK_NB) != 0,
+                    string("Lock file exists " + lock_file + ": ") + strerror(errno), errno);
+    }
+
+    static void reg_event_find_chromium_nodes(void *data, uint32_t id, uint32_t permissions, const char *type,
+                                              uint32_t version, const struct spa_dict *props) {
+
+        auto *reg_data = (struct registry_event_global_data *)data;
+
+        if (strcmp(type, PW_TYPE_INTERFACE_Node) == 0) {
+            for (uint32_t i = 0; i < props->n_items; i++) {
+
+                if (strcmp(props->items[i].key, PW_KEY_APP_NAME) != 0)
+                    continue;
+
+                if (strcmp(props->items[i].value, "Chromium") == 0 ||
+                    strcmp(props->items[i].value, "Chromium input") == 0) {
+
+                    NodesManager::process_new_node(reg_data->reg, pw_main_loop_get_loop(reg_data->main_loop), id, type);
+                }
+
+                break;
+            }
+        }
+    }
+
   public:
     static void start() {
+        checkLock();
+
         pw_init(nullptr, nullptr);
 
         // getting context to connect to pipewire daemon
@@ -66,7 +84,7 @@ class Daemon {
 
         // connect context to daemon
         struct pw_core *core = pw_context_connect(context, nullptr, 0);
-        raiseError(core == nullptr, string("failed to connect to pipewire daemon, ") + strerror(errno), errno);
+        raise_error(core == nullptr, string("failed to connect to pipewire daemon, ") + strerror(errno), errno);
 
         struct pw_registry *registry = pw_core_get_registry(core, PW_VERSION_REGISTRY, 0);
 
